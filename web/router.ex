35bc8606b7d812b9acff071b09285589f527157a
David Richards <davidlamontrichards@gmail.com>
2016-10-27 09:57:45 -0600

    WIP

--- web/router.ex
+++ web/router.ex
@@ -26,9 +26,6 @@ defmodule Skylab.Router do
   end
 
   if Mix.env == :dev do
-    # forward "/docs", Absinthe.Plug.GraphiQL, schema: Skylab.Schema,
-    #   graphql_endpoint: 'http://localhost:4000/graphql',
-    #   headers: %{"authorization" => "BEARER eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJVU0s1N25mc3Jjd3Zmdm5aYXl5N3VMUEJuTUtHQ2RXeCIsInN1YiI6ImF1dGgwfDU3YjFmYWY5NzFjMTZjZTg3NGI5NGVkMiJ9.z_OTBfmOWlVqJL2OC0ku3XDy7OhuMy7ukTb6tGQjjak"}
     forward "/docs", Absinthe.Plug.GraphiQL, schema: Skylab.Schema
   end
 end
